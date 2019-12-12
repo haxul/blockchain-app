98060b4c5378e0d0ab9bb6331ab1b6b14c1ad2f2
haxul <41199611+haxul@users.noreply.github.com>
2019-12-12 22:57:03 +0400

    Update dappgame.cpp

--- backend/src/dappgame.cpp
+++ backend/src/dappgame.cpp
@@ -3,9 +3,14 @@
 
 
 
-ACTION dappgame::hi(name user) {
-    require_auth(user);
-    print("hello ", name{user});
+ACTION dappgame::login(name user) {
+  require_auth(user);
+  auto user_iterator = _users.find(user.value);
+  if (user_iterator == _users.end()) {
+    _users.emplace(user, [&](auto& new_user) {
+         new_user.username = user;
+      });
+  }
 }
 
-EOSIO_DISPATCH(dappgame, (hi))
+EOSIO_DISPATCH(dappgame, (login))
