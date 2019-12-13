ea921746a946f4c951757875eb84456ee4da677e
haxul <s.s.starodubov@yandex.ru>
2019-12-14 00:11:29 +0400

    feat: create button component

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Button = () => (
+  <button
+    className="ctrl-standard is-reversed typ-subhed fx-bubbleUp "
+    type="submit"
+  >
+    Login
+  </button>
+)
