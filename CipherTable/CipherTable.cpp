c7ed885ae30afe983be9b6e578fc8f9dfce58436
haxul <s.s.starodubov@yandex.ru>
2019-12-16 20:55:50 +0400

    init

---
+++ CipherTable/CipherTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by haxul on 12/16/19.
+//
+
+#include "CipherTable.h"
---
+++ CipherTable/CipherTable.h
@@ -0,0 +1,14 @@
+//
+// Created by haxul on 12/16/19.
+//
+
+#ifndef BLOCKCHAIN_GAME_CIPHERTABLE_H
+#define BLOCKCHAIN_GAME_CIPHERTABLE_H
+
+
+class CipherTable {
+
+};
+
+
+#endif //BLOCKCHAIN_GAME_CIPHERTABLE_H
