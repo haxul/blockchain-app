15b81477d65aa7c1f4a5f67890d6b6f045f8f454
haxul <s.s.starodubov@yandex.ru>
2019-12-18 20:59:51 +0400

    feat: create function to fill

--- Key/Key.cpp
+++ Key/Key.cpp
@@ -1,20 +1,24 @@
+#include <iostream>
 #include "Key.h"
 #include "string"
 
 using namespace std;
 
 Key::Key(string key_value) {
-    value = new string(key_value);
+    this->key_value = new string(key_value);
 }
 
 Key::~Key() {
-    delete value;
+    delete key_value;
 }
 
-string Key::fill_text_by_key(int size) {
+string Key::fill_text_by_key(unsigned int size) {
     string result = "";
-    for (int i = 0; i < size ; ++i) {
-
+    int key_letter_index = 0;
+    for (int i = 0; ; ++i) {
+        if (result.size() >= size) return result;
+        if (key_letter_index >= (*key_value).size()) key_letter_index = 0;
+        result += (*key_value).at(key_letter_index);
+        key_letter_index++;
     }
-    return reinterpret_cast<basic_string<char, char_traits<char>, allocator<char>> &&>((*value)[0]);
 }
