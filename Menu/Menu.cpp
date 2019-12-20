c194fffc2bff7e760bbe0c2c1fcb32904cf8232a
haxul <s.s.starodubov@yandex.ru>
2019-12-20 21:29:24 +0400

    feat: add file enter

--- Menu/Menu.cpp
+++ Menu/Menu.cpp
@@ -1,23 +1,26 @@
 #include "Menu.h"
 #include <iostream>
 #include <regex>
+#include <fstream>
 
 #define AMOUNT_CHARS 32767
 #define ENTER_TEXT_MANUALLY 0
 #define INPUT_FILE_TEXT 1
+#define EXIT 2
 using namespace std;
 
 void Menu::open() {
     cout << "Hello, please choose how do you want to enter text data?" << endl;
     cout << "0. Enter text manually by keyboard" << endl;
     cout << "1. Enter text by existed file" << endl;
+    cout << "2. Exit" << endl;
 }
 
 unsigned int Menu::choose_option_to_input_text() {
     while (true) {
         unsigned int choosen_option;
         cin >> choosen_option;
-        bool isCorrectCommand = choosen_option == INPUT_FILE_TEXT || choosen_option == ENTER_TEXT_MANUALLY;
+        bool isCorrectCommand = choosen_option == INPUT_FILE_TEXT || choosen_option == ENTER_TEXT_MANUALLY || choosen_option == EXIT;
         bool isCorrectEnter = !cin.fail() && isCorrectCommand;
         if (isCorrectEnter) return choosen_option;
         cout << "Unknown command, please try again:" << endl;
@@ -34,9 +37,21 @@ string Menu::enter_text(const string message) {
     return text;
 }
 
-string Menu::input_file()   {
-    //TODO
-    return "file";
+string Menu::read_file() {
+    ifstream file;
+    string path;
+    cout << "enter file path: " << endl;
+    cin >> path;
+    file.open(path);
+    string result;
+    if (!file.is_open()) return "";
+    while (!file.eof()) {
+        string bytes;
+        file >> bytes;
+        result += bytes;
+    }
+    file.close();
+    return result;
 }
 
 string Menu::get_key() {
@@ -50,5 +65,9 @@ string Menu::get_key() {
 
 string Menu::get_text() {
     unsigned int choosed_option = choose_option_to_input_text();
-    return choosed_option == ENTER_TEXT_MANUALLY ? enter_text("enter text to be encrypted: ") : input_file();
+    if (choosed_option == EXIT) {
+        cout << "bay :)" << endl;
+        exit(1);
+    }
+    return choosed_option == ENTER_TEXT_MANUALLY ? enter_text("enter text to be encrypted: ") : read_file();
 }
--- Menu/Menu.h
+++ Menu/Menu.h
@@ -8,7 +8,7 @@ class Menu {
 private:
     unsigned int choose_option_to_input_text();
     string enter_text(const string message);
-    string input_file();
+    string read_file();
 public:
     void open();
     string get_key();
